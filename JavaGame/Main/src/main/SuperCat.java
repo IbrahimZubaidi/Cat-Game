e3c9bc29c733144040ae86b947829d3e4e0ff53e
janaellayan <156193133+janaellayan@users.noreply.github.com>
2025-01-13 13:03:35 +0300

    Delete JavaGame/Main/src/main/SuperCat.java

--- JavaGame/Main/src/main/SuperCat.java
+++
@@ -1,7 +0,0 @@
-package main;
-        
-public class SuperCat extends Cat{
-    SuperCat(double positionX, double positionY, double width, double height, double speedX, double speedY, String spritePath, boolean isCollidable, boolean isVisible, int lives, int score){
-        super(positionX, positionY, width, height, speedX, speedY, spritePath, isCollidable, isVisible, lives, score);
-    }
-}
